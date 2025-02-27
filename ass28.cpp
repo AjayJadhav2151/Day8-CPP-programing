d9194a707d1e57dac0110b201f8c2f1bd287b1fc
AjayJadhav2151 <ajayjadhav.skn.comp@gmail.com>
2025-02-27 20:52:45 +0530

    Adding data of Day 8 of CDAC Cpp Programing Assignment number 28

---
+++ ass28.cpp
@@ -0,0 +1,11 @@
+//28.	Write a program which will calculate multiplication of two 3 by 3 matrices.
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+int main() {
+	int matrice1[3][3] = { 1,2,3,4,5,6,7,8,9 };
+	int matrice2[3][3] = { 1,2,3,4,5,6,7,8,9 };
+	int matrice3[3][3];
+	for (int i = 0; i <= 3; i++) {
+
+	}
+}
