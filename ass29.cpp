c5364702e33548951d1aa28c993e8219ec6169f7
AjayJadhav2151 <ajayjadhav.skn.comp@gmail.com>
2025-02-28 23:05:15 +0530

    adding Day8 of CPP programing Assignment29

---
+++ ass29.cpp
@@ -0,0 +1,5 @@
+//29.	Write a program to accept a string from the user.Write separate functions to perform the following :
+//a.Find the number of occurrences of given character in the string
+//b.Find the number of blank spaces in the string.
+//c.Find the number of words in the string.
+//d.Find the total number of all the vowels in the string.
